c085c19333f7dcfa7e41d0cfdb35e1aa51658843
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2023-09-16 10:28:03 +0200

    Added

---
+++ IPWho.is_Test.dpr
@@ -0,0 +1,33 @@
+﻿program IPWho.is_Test;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  System.DateUtils,
+  IPWhoAPI in 'API\IPWhoAPI.pas';
+
+procedure Main;
+begin
+  var ipInfo := TIPWho.GetIPInfo();
+  Writeln(ipInfo.ip);
+  Writeln('Raw Data:');
+  Writeln(ipInfo.rawData);
+end;
+
+begin
+  try
+    Main;
+
+    ReadLn;
+  except
+    on E: Exception do
+    begin
+      Writeln(E.ClassName, ': ', E.Message);
+      ReadLn;
+    end;
+  end;
+end.
+
